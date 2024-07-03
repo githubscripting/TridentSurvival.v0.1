cc92b21b014ede6a9dbb8f1a9606f86efd19422b
githubscripting <nngithub1scripts@gmail.com>
2024-07-03 19:42:15 +0300

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -9,17 +9,20 @@ print'╚█████╗░██╔██╗██║█████╗░�
 print'░╚═══██╗██║╚████║██╔══╝░░╚═╝██║░░░░╚██╔╝░░░░░██╔═══╝░██║░░░██║██╔══██╗'
 print'██████╔╝██║░╚███║███████╗███████╗░░░██║░░░██╗██║░░░░░╚██████╔╝██████╦╝'
 print'╚═════╝░╚═╝░░╚══╝╚══════╝╚══════╝░░░╚═╝░░░╚═╝╚═╝░░░░░░╚═════╝░╚═════╝░'
+--------------Hitbox----------------
+Tab:AddButton({ Name = "ESP&hitboxHEAD(rage=7zxy; possible ban)", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/githubscripting/TridentSurvival.v0.1/main/HB-HEAD-7.lua", true))() end })
 ------------------------------
-Tab:AddButton({ Name = "ESP&hitboxHEAD(rage=7zxy; possible ban)", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/NNlove101011/test2/main/lua", true))() end })
+Tab:AddButton({ Name = "ESP&hitboxHEAD(small heads=3zxy; possible ban)", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/githubscripting/TridentSurvival.v0.1/main/HB-HEAD-Legit-3.lua", true))() end })
 ------------------------------
-Tab:AddButton({ Name = "ESP&hitboxHEAD(small heads=5zxy; possible ban)", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/NNlove101011/test2/main/lua", true))() end })
+Tab:AddButton({ Name = "ESP&hitbox body(medium=7 zxy; possible ban)", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/githubscripting/TridentSurvival.v0.1/main/HB-body-big.lua", true))() end })
 ------------------------------
-Tab:AddButton({ Name = "ESP&hitbox body(medium=7 zxy; possible ban)", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/NNlove101011/Hitbox-/main/lua", true))() end})
+Tab:AddButton({ Name = "ESP&hitbox body(medium=5 zxy; possible ban)", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/githubscripting/TridentSurvival.v0.1/main/Create%20HB-Torso-5.lua", true))() end})
 ------------------------------
-Tab:AddButton({ Name = "ESP&Hitbox body(legit)", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/NNlove101011/Hitvox-legit/main/lua", true))() end})
-------------------------------
+Tab:AddButton({ Name = "ESP&Hitbox body(legit)", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/githubscripting/TridentSurvival.v0.1/main/Create%20HB-Torso-3.9.lua", true))() end})
+---------------Visual----------------
 local Tab = Window:MakeTab({ Name = "Visual", Icon = "rbxassetid://4483345998", PremiumOnly = false })
 ------------------------------
 Tab:AddButton({ Name = "Night vision", Callback = function() local Light = game:GetService("Lighting") function dofullbright() Light.Ambient = Color3.new(1, 1, 1) Light.ColorShift_Bottom = Color3.new(1, 1, 1) Light.ColorShift_Top = Color3.new(1, 1, 1)end dofullbright() Light.LightingChanged:Connect(dofullbright) end }) --[[ Name = - The name of the button. Callback = - The function of the button. ]]
 ------------------------------
-Tab:AddButton({ Name = "Crosshair", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Crosshair-Script/main/Crosshair.lua"))() end})
+Tab:AddButton({ Name = "Crosshair", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Crosshair-Script/main/Crosshair.lua"))() end })
+------------------------------
